f176dd35510a20d423b4ec418ef835203b8bc226
mymth <miyamo.4@gmail.com>
2021-01-17 18:54:52 -0500

    bugfix: inline picker submits form by click on prev/next buttons Fix #48

--- dist/js/datepicker-full.js
+++ dist/js/datepicker-full.js
@@ -762,16 +762,16 @@
     <div class="datepicker-header">
       <div class="datepicker-title"></div>
       <div class="datepicker-controls">
-        <button class="%buttonClass% prev-btn"></button>
-        <button class="%buttonClass% view-switch"></button>
-        <button class="%buttonClass% next-btn"></button>
+        <button type="button" class="%buttonClass% prev-btn"></button>
+        <button type="button" class="%buttonClass% view-switch"></button>
+        <button type="button" class="%buttonClass% next-btn"></button>
       </div>
     </div>
     <div class="datepicker-main"></div>
     <div class="datepicker-footer">
       <div class="datepicker-controls">
-        <button class="%buttonClass% today-btn"></button>
-        <button class="%buttonClass% clear-btn"></button>
+        <button type="button" class="%buttonClass% today-btn"></button>
+        <button type="button" class="%buttonClass% clear-btn"></button>
       </div>
     </div>
   </div>
--- js/picker/templates/pickerTemplate.js
+++ js/picker/templates/pickerTemplate.js
@@ -5,16 +5,16 @@ const pickerTemplate = optimizeTemplateHTML(`<div class="datepicker">
     <div class="datepicker-header">
       <div class="datepicker-title"></div>
       <div class="datepicker-controls">
-        <button class="%buttonClass% prev-btn"></button>
-        <button class="%buttonClass% view-switch"></button>
-        <button class="%buttonClass% next-btn"></button>
+        <button type="button" class="%buttonClass% prev-btn"></button>
+        <button type="button" class="%buttonClass% view-switch"></button>
+        <button type="button" class="%buttonClass% next-btn"></button>
       </div>
     </div>
     <div class="datepicker-main"></div>
     <div class="datepicker-footer">
       <div class="datepicker-controls">
-        <button class="%buttonClass% today-btn"></button>
-        <button class="%buttonClass% clear-btn"></button>
+        <button type="button" class="%buttonClass% today-btn"></button>
+        <button type="button" class="%buttonClass% clear-btn"></button>
       </div>
     </div>
   </div>
