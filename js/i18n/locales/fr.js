4589fd8c5c8c669567a797599b2ebd63c1337e6a
Romain Monteil <monteil.romain@gmail.com>
2023-09-26 12:02:13 +0200

    Update FR translation

--- js/i18n/locales/fr.js
+++ js/i18n/locales/fr.js
@@ -1,14 +1,18 @@
 /**
- * French translation for bootstrap-datepicker
+ * French translation for vanillajs-datepicker
+ *
+ * Based on French translation for bootstrap-datepicker
  * Nico Mollet <nico.mollet@gmail.com>
+ *
+ * @see https://fr.wikipedia.org/wiki/Mois
  */
 export default {
   fr: {
     days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"],
     daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."],
-    daysMin: ["d", "l", "ma", "me", "j", "v", "s"],
+    daysMin: ["D", "L", "M", "M", "J", "V", "S"],
     months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
-    monthsShort: ["janv.", "févr.", "mars", "avril", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
+    monthsShort: ["janv.", "févr.", "mars", "avr.", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
     today: "Aujourd'hui",
     monthsTitle: "Mois",
     clear: "Effacer",
